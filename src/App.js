cde33b15d029d48f63ab72fb376c8287fa42f910
zty <zhangtengyue@qbb6.com>
2020-03-19 21:19:48 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Index from "./views/Index"
+import Sums from "./views/sums/index"
 import {HashRouter, Route, Switch} from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <HashRouter>
           <Switch>
             <Route exact path="/" component={Index}></Route>
+            <Route exact path="/sums" component={Sums}></Route>
             {/* <Route component={noMatch}/>  */}
           </Switch>
       </HashRouter>
--- src/views/Index.js
+++ src/views/Index.js
@@ -4,51 +4,66 @@ import './Index.scss'
 
 import questions from 'assets/js/questions.js'
 
-var num = [2, 3, 4, 7, 11, 15], tar = 9
-var nums = [2, 7, 11, 15], target = 9
-var nums2 = [2, 7, 8, 11, 15], target2 = 18
-var nums3 = [2, 7, 9, 9, 11, 15], target3 = 18
-// 一个数 只可能是两个一大一下的数相加 或是 两个相等1/2 相加
-// 太多if else 性能不好
-var twoSum = function(nums, target) {
-  let small = nums.filter(x => x <= target/2)
-  let s_l = small.length
-  let large = nums.filter(x => x > target/2 && x < target)
-  let l_l = large.length
-  if (s_l === 0 && l_l === 0) {
-    return null
-  } else if (s_l === 0) {
-    return null
-  } else if (l_l === 0) {
-    if (s_l >= 2) {
-      if (small[s_l - 1] + small[s_l - 2] === target) {
-        return [s_l - 2, s_l - 1]
-      } else {
-        return null
-      }
-    } else {
-      return null
-    }
-  } else {
-    for(let i = 0; i < s_l; i++) {
-      let n = small[i]
-      let idx = large.findIndex(y => y === target - n)
-      if (idx !== -1) {
-        return [i, s_l + idx]
-      }
-    }
-    return null
+var reverse1 = function(x) {
+  let f = false
+  if (x < 0) {
+    f = true
   }
+  var str = f ? String(x).substring(1) : String(x)
+  var strList = str.split('').reverse()
+  for (var i = 0; i < strList.length; i++) {
+    if (strList[i] === '0') {
+      strList[i] = undefined
+    } else {
+      break; 
+    }
+  }
+  var str = strList.filter(x => x).join('')
+  var num = Number(str)
+  var number = f ? -num  : num
+  if (number < -Math.pow(2, 31) || number > Math.pow(2, 31) -1) {
+    number = 0
+  }
+  return number
 };
-console.log('num = [2, 3, 4, 7, 11, 15], tar = 9 expect [0, 3]', twoSum(num, tar))
-console.log('nums = [2, 7, 11, 15], target = 9 expect [0, 2]', twoSum(nums, target))
-console.log('nums2 = [2, 7, 8, 11, 15], target2 = 18 expect [1, 3]', twoSum(nums2, target2))
-console.log('nums3 = [2, 7, 9, 9, 11, 15], target3 = 18 expect [2, 3]', twoSum(nums3, target3))
 
-twoSum(num, tar)
-twoSum(nums, target)
-twoSum(nums2, target2)
-twoSum(nums3, target3)
+var reverse = function (x) {
+  let overflow = function (x) {
+    return (x >= Math.pow(2, 31) - 1 || x <= -Math.pow(2, 31)) ? 0 : x
+  }
+  x = overflow(x)
+
+  let isNegative = x < 0
+  if (isNegative) {
+    x = -x
+  }
+  let a = 0
+  while (x > 9) {
+    a = a * 10 + x % 10 * 10 // 求余 乘10
+    console.log('a = ', a)
+    x = parseInt(x / 10)
+    console.log('x = ', x)
+  }
+  a += x
+
+  return isNegative ? overflow(-a) : overflow(a)
+};
+
+// 108 ms	35.2 MB
+
+// 	80 ms	36.2 MB
+
+console.log('123 expected 321', reverse(123))
+// a = 30 x = 12
+// a = 320 x = 1
+// 相当于两辆火车 1. A的车尾去B的车尾  2. A的倒数第二 去 B 的车尾，原本B车尾*10上升一位。 3 上车采用加法
+
+// console.log('120 expected 21', reverse(120))
+// console.log('-123 expected -321', reverse(-123))
+// console.log('-120 expected -21', reverse(-120))
+// console.log('-(2 ^ 32)', reverse(-Math.pow(2, 32)))
+// console.log('2 ^ 33', reverse(Math.pow(2, 33)))
+
 
 export default class Index extends React.Component {
   UNSAFE_componentWillMount() {
@@ -56,12 +71,15 @@ export default class Index extends React.Component {
   componentDidMount() {
 
   }
+  goAnswer(item, e) {
+    this.props.history.push(`/${item.path}`)
+  }
   render() {
     return (
       <div className="page page-index">
         {
           questions.map((item, index) => <div className="card-box" key={index}>
-            <Card title={item.title}>
+            <Card title={item.title} hoverable={true} onClick={(e) => this.goAnswer(item, e)}>
               <div>{item.content}</div>
               <div>
               <Rate tooltips='难度' value={item.difficulty} />
