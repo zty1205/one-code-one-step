1bde31b85392b6c5ce1065d077160f47208b7286
张腾跃 <tengyuezhang@91jkys.com>
2021-07-04 16:51:25 +0800

    code 25, 30, 34, 45

--- leetCode/run.js
+++ leetCode/run.js
@@ -1,6 +1,8 @@
 function runList(input = [], res = [], func) {
   input.forEach((item, idx) => {
     const result = func(...item);
-    console.log(`input: ${item}, expect res ${res[idx]} === ${result}, isRight: ${result === res[idx]}`);
+    console.log(
+      `input: ${item}, expect res ${res[idx]} === ${result}, isRight: ${String(result) === String(res[idx])}`
+    );
   });
 }
