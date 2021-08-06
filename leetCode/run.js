d466eec68bc33ae67f4171fee2cd0c1dac616fbf
zty1205 <zty1160051490@163.com>
2021-08-06 18:03:35 +0800

    feat: remove runjs

--- leetCode/run.js
+++
@@ -1,8 +0,0 @@
-function runList(input = [], res = [], func) {
-  input.forEach((item, idx) => {
-    const result = func(...item);
-    console.log(
-      `input: ${item}, expect res ${res[idx]} === ${result}, isRight: ${String(result) === String(res[idx])}`
-    );
-  });
-}
