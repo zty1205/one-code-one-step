a0b049f0bf3feff155e25b6ad9b498dfefca0529
zty1205 <zty1160051490@163.com>
2022-02-08 22:26:57 +0800

    feat: help tree

---
+++ helper/tree.js
@@ -0,0 +1,29 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
+function buildTreeByArray(array) {
+  if (!array || !Array.isArray(array)) return null;
+  array.reverse();
+  const root = new TreeNode(array.pop());
+  const stack = [root];
+  while (array.length) {
+    let node = stack.shift();
+    let leftVal = array.pop();
+    let rightVal = array.pop();
+    if (node != null) {
+      let left = leftVal != null ? new TreeNode(leftVal) : null;
+      let right = rightVal != null ? new TreeNode(rightVal) : null;
+      node.left = left;
+      node.right = right;
+      stack.push(left);
+      stack.push(right);
+    }
+  }
+  return root;
+}
+
+window.buildTreeByArray = buildTreeByArray;
