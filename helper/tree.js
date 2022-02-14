90043791cb41801aebbf5c63ccb52861dd1c0fb3
zty1205 <zty1160051490@163.com>
2022-02-14 21:12:45 +0800

    feat: helper tree map

--- helper/tree.js
+++ helper/tree.js
@@ -5,10 +5,21 @@ class TreeNode {
   }
 }
 
-function buildTreeByArray(array) {
+function _buildNodeMap(map = new Map(), node, config = {}) {
+  if (!node || !config.nodeMap) return;
+  map.set(node.val, node);
+}
+
+function buildTreeByArray(array, config = {}) {
   if (!array || !Array.isArray(array)) return null;
+
+  const nodeMap = new Map();
+
   array.reverse();
   const root = new TreeNode(array.pop());
+
+  _buildNodeMap(nodeMap, root, config);
+
   const stack = [root];
   while (array.length) {
     let node = stack.shift();
@@ -17,11 +28,15 @@ function buildTreeByArray(array) {
     if (node != null) {
       let left = leftVal != null ? new TreeNode(leftVal) : null;
       let right = rightVal != null ? new TreeNode(rightVal) : null;
+
+      _buildNodeMap(nodeMap, left, config);
+      _buildNodeMap(nodeMap, right, config);
+
       node.left = left;
       node.right = right;
       stack.push(left);
       stack.push(right);
     }
   }
-  return root;
+  return config.nodeMap ? { root, nodeMap } : root;
 }
